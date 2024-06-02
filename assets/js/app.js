c075671fc470c36d1e9e9bdbbb9bb87437b4957f
João José Cardoso Ribeiro <joaojosecr@gmail.com>
2024-06-02 17:34:34 -0300

    cadastro de veiculo

---
+++ assets/js/app.js
@@ -0,0 +1,95 @@
+const servidor = "http://localhost:3000"
+
+function cadastrar_veiculo(event) {
+    event.preventDefault();
+    
+    const form = document.querySelector("#formulario");
+
+    const data_e = get_data();
+    const hora_e = get_hora();
+    const placa = document.querySelector("placaVeiculo");
+    const t_veiculo = document.querySelector("tipoVeiculo");
+    const t_cliente = document.querySelector("tipoCliente");   
+        
+    invalido = 0;
+    
+        
+    if(placa.value===""){
+        alert("Por favor, preencha a placa.");
+        invalido = 1;
+    } 
+
+    if (invalido == 0){
+
+        const veiculo = {
+            "data_entrada":data_e,
+            "hora_entrada":hora_e,
+            "data_saida": null,
+            "hora_saida":null,  
+            "placa":placa,  
+            "tipo_veiculo":t_veiculo,
+            "id_cliente":1,
+            "tipo_cliente":t_cliente
+        };
+    
+        
+    
+        console.log(JSON.stringify(veiculo));
+        const url = servidor+"/veiculos"
+        fetch(url, {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/json',
+            },
+            body: JSON.stringify(veiculo),
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Dados enviados com sucesso:', data);
+            alert("Veiculo cadastrado com sucesso!");
+            //window.location.href = '../index.html';
+        })
+        .catch(error => {
+            console.error('Erro ao enviar dados:', error);
+        });         
+    
+    }
+
+}
+
+function formatTimeUnit(unit) {
+    return unit < 10 ? '0' + unit : unit;
+}
+
+function get_data() {
+    const now = new Date();
+    
+    // Obter partes da data
+    const year = now.getFullYear();
+    const month = formatTimeUnit(now.getMonth() + 1); // Os meses são de 0 a 11
+    const day = formatTimeUnit(now.getDate());
+    // Formatar a data e hora
+    const date = `${day}/${month}/${year}`;
+    
+    // Exibir a data e hora
+    //document.getElementById('datetime').textContent = `${date} ${time}`;
+    return date;
+
+}
+
+function get_hora() {
+    const now = new Date();
+    
+    // Obter partes da hora
+    const hours = formatTimeUnit(now.getHours());
+    const minutes = formatTimeUnit(now.getMinutes());
+    const seconds = formatTimeUnit(now.getSeconds());
+
+    // Formatar a data e hora
+    
+    const time = `${hours}:${minutes}:${seconds}`;
+    
+    // Exibir a data e hora
+    //document.getElementById('datetime').textContent = `${date} ${time}`;
+    return time;
+}
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,6 @@
-document.getElementById('formulario').addEventListener('submit', cadastrarVeiculo);
+const servidor = "http://localhost:3000"
+
+//document.getElementById('formulario').addEventListener('submit', cadastrarVeiculo);
 
 function cadastrarVeiculo(e) {
     var placaVeiculo = document.getElementById('placaVeiculo').value;
@@ -12,25 +14,45 @@ function cadastrarVeiculo(e) {
         return false;
     }
 
-    var veiculo = {
-        placa: placaVeiculo,
-        tipo: tipoVeiculo,
-        cliente: tipoCliente,
-        hora: time.getHours(),
-        minutos: time.getMinutes()
+    const data_e = get_data();
+    const hora_e = get_hora();
+
+    
+    const veiculo = {
+        "data_entrada":data_e,
+        "hora_entrada":hora_e,
+        "data_saida": null,
+        "hora_saida":null,  
+        "placa":placaVeiculo,  
+        "tipo_veiculo":tipoVeiculo,
+        "id_cliente":'1',
+        "tipo_cliente":tipoCliente
     };
 
-    if (localStorage.getItem('patio') === null) {
-        var veiculos = [];
-        veiculos.push(veiculo);
-        localStorage.setItem('patio', JSON.stringify(veiculos));
-    } else {
-        var veiculos = JSON.parse(localStorage.getItem('patio'));
-        veiculos.push(veiculo);
-        localStorage.setItem('patio', JSON.stringify(veiculos));
-    }
+    
 
-    document.getElementById('formulario').reset();
+    console.log(JSON.stringify(veiculo));
+
+    const url = servidor
+    fetch("http://localhost:3000/veiculos", {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(veiculo),
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log('Dados enviados com sucesso:', data);
+        alert("Veiculo cadastrado com sucesso!");
+        //window.location.href = '../index.html';
+    })
+    .catch(error => {
+        console.error('Erro ao enviar dados:', error);
+    });         
+    
+
+    //document.getElementById('formulario').reset();
 
     e.preventDefault();
 }
@@ -51,21 +73,71 @@ function removeVeiculo(placa){
 }
 
 function mostraPatio() {
-    var veiculos = JSON.parse(localStorage.getItem('patio'));
-    var patioResultado = document.getElementById('resultados');
+    alert("teste");
+    fetch("http://localhost:3000/veiculos")
+        .then(response => response.json())
+        .then(veiculos => {          
+            //console.log(pets)
+            
+            
+            for (let i = 0; i < veiculos.length; i++) {
+                let str = '';
+                if(veiculo.data_saida==null){
+                    saida = '<button class="calcular-saida-btn">Calcular Saída</button>'
+                } else {
+                    saida = str(veiculo.data_entrada) +  str(veiculo.hora_entrada)
+                }
+            
+                let veiculo = veiculos[i];
+                str+= `
+                <tr>
+                    <td>${veiculo.placa}</td>
+                    <td>${veiculo.tipo_veiculo}</td>
+                    <td>${veiculo.tipo_cliente}</td>
+                    <td>${veiculo.data_entrada} ${veiculo.hora_entrada}</td>
+                    <td>${saida}</td>
+                </tr>`
+                
+                document.querySelector('#resultados').innerHTML=str
+            }
+        });
+}
 
-    patioResultado.innerHTML = '';
 
-    for (var i = 0; i < veiculos.length; i++) {
-        var placa = veiculos[i].placa;
-        var tipo = veiculos[i].tipo;
-        var cliente = veiculos[i].cliente;
-        var hora = veiculos[i].hora;
-        var minutos = veiculos[i].minutos;
-        patioResultado.innerHTML += '<tr><td>' + placa + '</td>' +
-            '<td>' + tipo + '</td>' + '<td>' + cliente + '</td>' +
-            '<td>' + hora + ':' + minutos + '</td>' +
-            '<td><button onclick="removeVeiculo(\'' + placa + '\')" class="btn btn-danger">Marcar Saída</button></td>' +
-            '</tr>';
-    }
+
+function formatTimeUnit(unit) {
+    return unit < 10 ? '0' + unit : unit;
+}
+
+function get_data() {
+    const now = new Date();
+    
+    // Obter partes da data
+    const year = now.getFullYear();
+    const month = formatTimeUnit(now.getMonth() + 1); // Os meses são de 0 a 11
+    const day = formatTimeUnit(now.getDate());
+    // Formatar a data e hora
+    const date = `${day}/${month}/${year}`;
+    
+    // Exibir a data e hora
+    //document.getElementById('datetime').textContent = `${date} ${time}`;
+    return date;
+
+}
+
+function get_hora() {
+    const now = new Date();
+    
+    // Obter partes da hora
+    const hours = formatTimeUnit(now.getHours());
+    const minutes = formatTimeUnit(now.getMinutes());
+    const seconds = formatTimeUnit(now.getSeconds());
+
+    // Formatar a data e hora
+    
+    const time = `${hours}:${minutes}:${seconds}`;
+    
+    // Exibir a data e hora
+    //document.getElementById('datetime').textContent = `${date} ${time}`;
+    return time;
 }
